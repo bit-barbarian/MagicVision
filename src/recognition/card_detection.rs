9edd097b6cc8f3d71704574e717c5d20bca7424a
Bit Barbarian <attempt.chaos233@addy.io>
2026-06-27 03:47:15 -0400

    fix simple ordering logic and remove needlessly complex version.

--- src/recognition/card_detection.rs
+++ src/recognition/card_detection.rs
@@ -3,7 +3,6 @@ use opencv::{
     core::{Mat, Point, Point2f, Scalar, Size, Vector},
     imgproc,
 };
-use std::cmp::Ordering;
 
 pub struct CardDetection {
     pub contour: Vector<Point>,
@@ -25,50 +24,6 @@ impl CardDetection {
     }
 
     fn order_corners(corners: [Point2f; 4]) -> [Point2f; 4] {
-        // Work on native vec to use Rust sorting functions
-        let mut points = corners;
-
-        let centroid = Point2f::new(
-            points.iter().map(|p| p.x).sum::<f32>() / 4.0,
-            points.iter().map(|p| p.y).sum::<f32>() / 4.0,
-        );
-
-        // Sort by angle around centroid.
-        points.sort_by(|a, b| {
-            let angle_a = (a.y - centroid.y).atan2(a.x - centroid.x);
-            let angle_b = (b.y - centroid.y).atan2(b.x - centroid.x);
-
-            angle_a.partial_cmp(&angle_b).unwrap_or(Ordering::Equal)
-        });
-
-        // Rotate so top-left comes first
-        let tl = points
-            .iter()
-            .enumerate()
-            .min_by(|(_, a), (_, b)| {
-                (a.x + a.y)
-                    .partial_cmp(&(b.x + b.y))
-                    .unwrap_or(Ordering::Equal)
-            })
-            .unwrap()
-            .0;
-        points.rotate_left(tl);
-
-        // Ensure clockwise ordering
-        let signed_area: f32 = points
-            .iter()
-            .zip(points.iter().cycle().skip(1))
-            .take(4)
-            .map(|(a, b)| a.x * b.y - b.x * a.y)
-            .sum();
-        if signed_area < 0.0 {
-            points.swap(1, 3);
-        }
-
-        [points[0], points[1], points[2], points[3]]
-    }
-
-    fn _order_corners_simple(corners: [Point2f; 4]) -> [Point2f; 4] {
         let points = corners;
 
         let mut tl = points[0];
@@ -83,7 +38,7 @@ impl CardDetection {
 
         for p in points {
             let sum = p.x + p.y;
-            let diff = p.x - p.y;
+            let diff = p.y - p.x;
 
             if sum < min_sum {
                 min_sum = sum;
