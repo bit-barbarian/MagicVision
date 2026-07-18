915ebe0ce2397b54f3a494f4c89b4133ec16dadf
Bit Barbarian <attempt.chaos233@addy.io>
2026-07-18 17:33:13 -0400

    remove old data files when downloading new from scryfall

--- src/bin/refresh-database/main.rs
+++ src/bin/refresh-database/main.rs
@@ -1,8 +1,7 @@
 mod image_proc;
 mod scryfall;
 use magicvision::cache::paths::get_data_dir;
-use std::path::{Path, PathBuf};
-use std::str::FromStr;
+use std::{path::PathBuf, time::SystemTime};
 use tokio::fs;
 
 use crate::image_proc::update_cache_with_jobs;
@@ -52,6 +51,12 @@ async fn main() -> DynResult<()> {
     let jobs = update_images(&json_filepath).await?;
     println!("Done updating image cache.");
 
+    if need_new_download {
+        println!("Cleaning up old Scryfall files...");
+        clean_old_raw_data_files().await?;
+        println!("Done.")
+    }
+
     println!("Loading hash cache...");
     let mut cache = load_card_cache()?;
     println!("Hash cache loaded.");
@@ -81,5 +86,34 @@ async fn read_stored_url() -> DynResult<Option<String>> {
 async fn write_stored_url(url: &str) -> DynResult<()> {
     let path = get_data_dir().join("last_downloaded_url.txt");
     atomic_write(&path, url.as_bytes()).await?;
+    Ok(())
+}
+
+async fn clean_old_raw_data_files() -> DynResult<()> {
+    let mut entries = fs::read_dir(get_data_dir()).await?;
+    let mut target_files: Vec<(PathBuf, SystemTime)> = Vec::new();
+
+    // Collect valid .ndjson files' path and modified time
+    while let Some(entry) = entries.next_entry().await? {
+        let path = entry.path();
+
+        if path.extension().is_some_and(|ext| ext == "ndjson") {
+            // Get metadata
+            if let Ok(meta) = entry.metadata().await
+                && let Ok(modified_time) = meta.modified()
+            {
+                target_files.push((path, modified_time));
+            }
+        }
+    }
+
+    target_files.sort_by_key(|(_, time)| *time);
+
+    if !target_files.is_empty() {
+        for (path, _) in target_files.iter().take(target_files.len() - 1) {
+            fs::remove_file(path).await?;
+        }
+    }
+
     Ok(())
 }
